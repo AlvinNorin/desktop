15b51636dd6e9e847b0702f95c6a5e72110300a2
AlvinNorin <alvin.norin@gmail.com>
2016-10-17 13:33:20 +0200

    Initial

---
+++ src/it/livingfor/livingforit/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package it.livingfor.livingforit.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import it.livingfor.livingforit.Livingforit;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Livingforit(), config);
+	}
+}
